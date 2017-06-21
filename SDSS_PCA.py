b2e3aa3ae4155eaf26cfafcd685da831681b9686
Nick Rumbaugh <drnickrumbaugh@gmail.com>
2017-06-20 21:33:08 -0700

    Initial commit

---
+++ SDSS_PCA.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import load_spec_files as LSF
+from sklearn import decomposition,neighbors
+from sklearn.neighbors import KDTree,BallTree
+
+class SDSS_PCA:
+    def __init__(self,masterfile='./random_SDSS_specs.csv',fluxdf=None,inputfile=None):
+        self.master=pd.read_csv(masterfile)
+        if fluxdf!=None:
+            self.fluxdf=fluxdf
+        elif inputfile!=None:
+            self.fluxdf=pd.read_csv(inputfile)
+
+    def set_flux(infile):
+        self.fluxdf=pd.read_csv(infile)
+
+    def load_spec_files(spec_dir='./spec_dir',smooth_wid=10,wavstep=None,wavmin=0,wavmax=10000,savefile=None):
+        redshifts,plates,mjds,fibers,ids=self.master.z.values,self.master.plate.values,self.master.mjd.values,self.master.fiberid.values,self.master.specobjid.values
+        self.fluxdf=LSF(redshifts,plates,mjds,fibers,spec_dir=spec_dir,smooth_wid=smooth_wid,wavstep=wavstep,wavmin=wavmin,wavmax=wavmax,savefile=savefile,ids=ids)
+
+    def DoPCA(self,n_components='mle'):
+        try:
+            self.fluxdf
+        except NameError:
+            print 'Must set fluxdf'
+            return
+        X = self.fluxdf.values
+        if n_components=='mle':
+            svd_solver == 'full'
+        else:
+            svd_solver='auto'
+        self.pca = decomposition.PCA(n_components=n_components,svd_solver=svd_solver)
+        self.pca.fit(X)
+        self.flux_pca=self.pca.transform(X)
+    
+    def KDTreeClassify(self,train_perc=0.9,n_neighbors=5, leaf_size=30, metric='euclidean',weights='distance'):
+        try:
+            self.flux_pca,self.master
+        except NameError:
+            print 'Must set flux_pca and master'
+            return
+        #try:
+        #    self.sourcetype
+        train_df=flux_pca.iloc[:int(train_perc*len(df))]
+        train_X,train_y=train_df.values,self.master['class'].values[:int(train_perc*len(df))]
+        test_X,test_y=flux_pca.iloc[int(train_perc*len(df)):].values,self.master['class'].values[int(train_perc*len(df)):]
+        self.clf=neighbors.KNeighborsClassifier(n_neighbors,weights=weights,algorithm='kdtree')
+        self.clf.fit(train_X,train_y)
+        self.predicted_y=self.clf.predict(test_X)
+        
+    def ComparePredictions(checkvalues=None,imax=None,verbose=False):
+        try:
+            self.predicted_y,self.master
+        except NameError:
+            print 'Must set predicted_y and master'
+            return
+        if check_values==None:check_values=self.master['class'].values[-len(predicted_y)]
+        if not(verbose):
+            perc_correct=np.sum(check_values==predicted_y)*1./len(predicted_y)*100
+            print 'Predicted Correctly: {:.2f}'.format(perc_correct)
+        else:
+            masterlen=len(self.master['class'].values)
+            if imax==None: imax=masterlen-len(self.predicted_y)
+            if imax>masterlen:imax=masterlen
+            for i in range(0,imax):
+                print check_values[i],predicted_y[i]
+            
---
+++ load_spec_files.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pyfits as py
+import pandas as pd
+import os
+
+
+def load_spec_files(redshifts,plates=None,mjds=None,fibers=None,spec_dir='./spec',smooth_wid=10,wavstep=None,wavmin=0,wavmax=10000,savefile=None,ids=None):
+    inpcheck=np.array([plates==None,mjds==None,fibers==None])
+    if inpcheck<3:
+        if inpcheck>0:
+            print 'plates, mjds, and fibers must ALL be provided'
+            try:
+                specfiles=os.listdir(spec_dir)
+            except OSError:
+                print "ls to the directory '{}' failed".format(spec_dir)
+                return
+            plates,mjds,fibers=np.zeros(len(specfiles),dtype='<i4'),np.zeros(len(specfiles),dtype='<i4'),np.zeros(len(specfiles),dtype='<i4')
+    else:
+        if ((len(plates)!=len(mjds))|(len(plates)!=len(fibers))):
+            print 'plates, fibers, and mjds must all have same length'
+            return
+        specfiles=np.array(['spec-{:04d}-{:05d}-{:04d}.fits'.format(x,y,z) for x,y,z in zip(plates,mjds,fibers)],dtype='|S25')
+    if wavstep==None: wavstep=smooth_wid*2
+    wavelengths=np.arange(wavmin,wavmax,wavstep)
+    fluxes=np.zeros((len(specfiles),len(wavelengths)))
+    for i in range(0,len(specfiles)):
+        spechdu=py.open('{}/{}'.format(spec_dir,specfiles[i]))
+        specdata=spechdu[1].data
+        tmp_flux,tmp_wav=specdata['flux'],10**(specdata['loglam'])
+        #Transform wavelength to rest-frame using redshifts
+        tmp_wav/=(1.+redshifts[i])
+        for wavcentmp,iw in zip(wavelengths,np.arange(len(wavelengths))):
+            fluxes[i][iw]=np.sum(tmp_flux[np.abs(tmp_wav-wavcentmp)<=smooth_wid])
+    fluxdf=pd.DataFrame(fluxes,columns=wavelengths)
+    if ids!=None:
+        fluxdf['ID']=ids
+        fluxdf.set_index('ID',inplace=True)
+    if savefile!=None:
+        if ids!=None:
+            fluxdf.to_csv(savefile)
+        else:
+            fluxdf.to_csv(savefile,index=False)
+    return fluxdf
